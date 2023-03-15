9f871e28c91de3adb3f36df317144db60188a595
therankofjedimaster <98640726+therankofjedimaster@users.noreply.github.com>
2023-03-15 17:29:55 +0000

    Update app.js
    Updated the js to make the time update correctly.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function updateLATime() {
         let losangelesHour = losangeles.querySelector(".time");
         let losangelesDay = losangeles.querySelector(".date");
         let losangelesTime = moment().tz("America/Los_Angeles");
-        losangelesHour.innerHTML = losangelesTime.format("HH:MM:ss");
+        losangelesHour.innerHTML = losangelesTime.format("HH:mm:ss");
         losangelesDay.innerHTML = losangelesTime.format("dddd, MMMM D, YYYY");
     }
 }
@@ -16,7 +16,7 @@ function updateRosarioTime() {
         let rosarioHour = rosario.querySelector(".time");
         let rosarioDay = rosario.querySelector(".date");
         let rosarioTime = moment().tz("America/Rosario");
-        rosarioHour.innerHTML = rosarioTime.format("HH:MM:ss");
+        rosarioHour.innerHTML = rosarioTime.format("HH:mm:ss");
         rosarioDay.innerHTML = rosarioTime.format("dddd, MMMM D, YYYY");
     }
 }
@@ -28,7 +28,7 @@ function updateDublinTime() {
         let dublinHour = dublin.querySelector(".time");
         let dublinDay = dublin.querySelector(".date");
         let dublinTime = moment().tz("Europe/Dublin");
-        dublinHour.innerHTML = dublinTime.format("HH:MM:ss");
+        dublinHour.innerHTML = dublinTime.format("HH:mm:ss");
         dublinDay.innerHTML = dublinTime.format("dddd, MMMM D, YYYY");
     }
 }
@@ -40,7 +40,7 @@ function updateIstanbulTime() {
         let istanbulHour = istanbul.querySelector(".time");
         let istanbulDay = istanbul.querySelector(".date");
         let istanbulTime = moment().tz("Asia/Istanbul");
-        istanbulHour.innerHTML = istanbulTime.format("HH:MM:ss");
+        istanbulHour.innerHTML = istanbulTime.format("HH:mm:ss");
         istanbulDay.innerHTML = istanbulTime.format("dddd, MMMM D, YYYY");
     }
 }
@@ -52,7 +52,7 @@ function updateTokyoTime() {
         let tokyoHour = tokyo.querySelector(".time");
         let tokyoDay = tokyo.querySelector(".date");
         let tokyoTime = moment().tz("Asia/Tokyo");
-        tokyoHour.innerHTML = tokyoTime.format("HH:MM:ss");
+        tokyoHour.innerHTML = tokyoTime.format("HH:mm:ss");
         tokyoDay.innerHTML = tokyoTime.format("dddd, MMMM D, YYYY");
     }
 }
